ede064986820d6e485d749c4a4407deee5cbe27b
Han Hero <97690445+HanHero4@users.noreply.github.com>
2022-02-14 10:18:57 +0800

    Update test_math.php

--- test_math.php
+++ test_math.php
@@ -867,7 +867,7 @@ if ($ans1 && $ans2) {
             <div id="container" style="height: 150px; margin-top: 1%;">
                 <div id="mini-container">
                     <br>
-                    <div class="exam-section" style="height: 100px;">
+                    <div class="exam-section" style="height: 75px;">
                         <p>COURSE RECOMMENDATION</p>
                     </div>
                 </div>
